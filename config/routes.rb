3c0adbede98eccb0fdaca3ae8533fdc2917e8907
James Reed <jamesreed.3050@gmail.com>
2022-05-17 14:27:23 +0900

    booking routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/', to: 'bookings#index'
-  get 'bookings/update'
-  get 'bookings/create'
+
 
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gears, only: [ :show ]
+  resource :bookings, only: [ :index, :update, :create ]
 end
