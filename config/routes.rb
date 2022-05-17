e5162170a7f08b78c3efa920475ff084cd3d0d5d
shingo kubomura <skksb75@gmail.com>
2022-05-17 12:37:36 +0900

    showpage done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'gears/index'
-  get 'gears/show'
-  get 'gears/new'
-  get 'gears/create'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gears, only: [ :show ]
 end
