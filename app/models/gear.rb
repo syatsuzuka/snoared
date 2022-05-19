4a27669678422138b97fed0c15d4cc40c360ffca
shingo kubomura <skksb75@gmail.com>
2022-05-19 12:07:02 +0900

    search feature added

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -4,4 +4,10 @@ class Gear < ApplicationRecord
   has_many :users, through: :bookings
   validates :title, :description, :price, :address, presence: true
 
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description_address,
+    against: [ :title, :description, :address ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
